0417359ddb4a7cc29d80e2084e42dcaa218e5ea3
Saumya Shreyashy <saumya2012@gmail.com>
2024-03-02 20:06:32 +0000

    Generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export let cart = [];
+export let cart = [{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 2
+}, {
+  productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+  quantity: 1
+}];
 
 export function addToCart (productId, addedMessageTimeoutId) {
   const quantitySelector = document.querySelector(`.quantity-selector-${productId}`);
